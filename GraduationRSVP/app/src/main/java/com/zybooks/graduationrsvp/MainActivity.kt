cae6844aa835c11948f1b90ebd64428e2a9891a3
Frank McCown <fmccown@harding.edu>
2023-12-28 10:40:12 -0600

    Add layout to GradScreen

--- GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -16,6 +26,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
 
@@ -38,20 +49,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
-   Image(
-      painter = painterResource(R.drawable.grad_cap),
-      contentDescription = null,
-      alpha = 0.3F
-   )
-   Text(
-      text = stringResource(R.string.graduation_announcement),
-      fontSize = 50.sp,
-      color = Color.Red,
-      textAlign = TextAlign.Center,
-      fontWeight = FontWeight.Bold,
-      lineHeight = 50.sp,
-      modifier = modifier
-   )
+   Column(
+      horizontalAlignment = Alignment.CenterHorizontally,
+      verticalArrangement = Arrangement.SpaceEvenly,
+      modifier = modifier.fillMaxHeight()
+   ) {
+      Text(
+         text = stringResource(R.string.graduation_announcement),
+         fontSize = 50.sp,
+         color = Color.Red,
+         textAlign = TextAlign.Center,
+         fontWeight = FontWeight.Bold,
+         lineHeight = 50.sp,
+         modifier = modifier
+      )
+      Image(
+         painter = painterResource(R.drawable.grad_cap),
+         contentDescription = null,
+         alpha = 0.3F
+      )
+      Row(verticalAlignment = Alignment.CenterVertically) {
+         Icon(
+            imageVector = Icons.Filled.Info,
+            contentDescription = "Information",
+            tint = Color.Gray,
+            modifier = Modifier.padding(10.dp).size(40.dp)
+         )
+         Text(
+            text = "May 14 - Ganus Hall - 2:00 pm",
+            fontSize = 20.sp
+         )
+      }
+   }
 }
 
 @Preview(showBackground = true)
