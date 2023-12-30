a9ea23c14d17c3b6b2f015babe80ec95e996bc5b
Frank McCown <fmccown@harding.edu>
2023-12-30 10:35:18 -0600

    Add Rsvp component

--- GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -16,8 +16,13 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -80,6 +85,29 @@ fun GradScreen(modifier: Modifier = Modifier) {
             fontSize = 20.sp
          )
       }
+      Rsvp(modifier)
+   }
+}
+
+@Composable
+fun Rsvp(modifier: Modifier = Modifier) {
+   var isChecked by remember { mutableStateOf(true) }
+   val message = if (isChecked) "I'll be there!" else "Sorry I can't be there."
+
+   Column(
+      modifier = modifier,
+      horizontalAlignment = Alignment.CenterHorizontally
+   ) {
+      Switch(
+         checked = isChecked,
+         onCheckedChange = {
+            isChecked = it
+         }
+      )
+      Text(
+         text = message,
+         fontSize = 30.sp
+      )
    }
 }
 
