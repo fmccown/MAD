b5a9808d22514ef976a63b62934f4a4270c29545
Frank McCown <fmccown@harding.edu>
2024-01-16 11:08:55 -0600

    Upgrade Gradle plugin

--- GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -40,7 +40,6 @@ class MainActivity : ComponentActivity() {
       super.onCreate(savedInstanceState)
       setContent {
          GraduationRSVPTheme {
-            // A surface container using the 'background' color from the theme
             Surface(
                modifier = Modifier.fillMaxSize(),
                color = MaterialTheme.colorScheme.background
