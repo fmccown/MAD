e70d7b3cb1954cfab0816cfe14bd15c4ddfb5e31
Frank McCown <fmccown@harding.edu>
2024-02-28 16:48:52 -0600

    Add PetAdoption_Starter

---
+++ PetAdoption_Starter/app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
@@ -0,0 +1,39 @@
+package com.zybooks.petadoption.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PetApp(
+   modifier: Modifier = Modifier
+) {
+   Scaffold(
+      topBar = {
+         PetAppBar()
+      }
+   ) { innerPadding ->
+      Text("Pet grid goes here", modifier.padding(innerPadding))
+   }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PetAppBar(
+   modifier: Modifier = Modifier
+) {
+   TopAppBar(
+      title = { Text("Pet Adoption") },
+      colors = TopAppBarDefaults.topAppBarColors(
+         containerColor = MaterialTheme.colorScheme.primaryContainer,
+         titleContentColor = MaterialTheme.colorScheme.primary,
+      ),
+      modifier = modifier
+   )
+}
