9da159efd4f3f607c6ada72f7f517bfd7c17309a
Frank McCown <fmccown@harding.edu>
2024-03-08 09:11:45 -0600

    Update GridDemo

--- GridDemo/app/src/main/java/com/zybooks/griddemo/MainActivity.kt
+++ GridDemo/app/src/main/java/com/zybooks/griddemo/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.lazy.staggeredgrid.LazyHorizontalStaggeredGri
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,7 +28,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,9 +44,6 @@ class MainActivity : ComponentActivity() {
                color = MaterialTheme.colorScheme.background
             ) {
                VerticalGrid()
-               //DemoHorizontalGrid()
-               //DemoVerticalStaggeredGrid()
-               //DemoHorizontalStaggeredGrid()
             }
          }
       }
@@ -67,10 +62,7 @@ fun LazyVerticalGridPreview() {
 @Composable
 fun VerticalGrid() {
    LazyVerticalGrid(
-      columns = GridCells.Adaptive(100.dp),
-      //columns = GridCells.Fixed(5),
-      //columns = GridCells.FixedSize(150.dp),
-      //modifier = Modifier.width(350.dp)
+      columns = GridCells.Adaptive(100.dp)
    ) {
       items(90) { item ->
          Card(
@@ -176,7 +168,7 @@ fun DemoVerticalStaggeredGrid() {
             Card(
                modifier = Modifier
                   .padding(4.dp)
-                  .size(sizes[item].dp)
+                  .height(sizes[item].dp)
             ) {
                Text(
                   text = "Item $item",
