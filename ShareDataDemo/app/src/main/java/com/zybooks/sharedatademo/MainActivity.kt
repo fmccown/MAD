a7648b3e565f0588712d8d6e77a40325e5bfd625
Frank McCown <fmccown@harding.edu>
2024-03-11 15:56:40 -0500

    Update ShareDataDemo

--- ShareDataDemo/app/src/main/java/com/zybooks/sharedatademo/MainActivity.kt
+++ ShareDataDemo/app/src/main/java/com/zybooks/sharedatademo/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.zybooks.sharedatademo
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.compose.foundation.layout.Column
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.zybooks.sharedatademo.ui.theme.ShareDataDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,8 +42,12 @@ fun ShareDataScreen(modifier: Modifier = Modifier) {
    val context = LocalContext.current
 
    Column {
-      Button(onClick = { shareMessage(context) }) {
-         Text("Share URL")
+      Button(onClick = {
+         val webpageUri = Uri.parse("https://www.zybooks.com/")
+         val intent = Intent(Intent.ACTION_VIEW, webpageUri)
+         context.startActivity(intent)
+      }) {
+         Text("View Webpage")
       }
       Button(onClick = { viewMap(context) }) {
          Text("View Map")
@@ -57,40 +58,58 @@ fun ShareDataScreen(modifier: Modifier = Modifier) {
       Button(onClick = { viewWebpage(context) }) {
          Text("View Webpage")
       }
+      Button(onClick = { noActivityFound(context) }) {
+         Text("No Activity Found")
+      }
+      Button(onClick = { shareMessage(context) }) {
+         Text("Share Message")
+      }
    }
 }
 
-private fun viewWebpage(context: Context) {
-   val webpage = Uri.parse("http://www.zybooks.com/")
+fun viewWebpage(context: Context) {
+   val webpage = Uri.parse("https://www.zybooks.com/")
    val intent = Intent(Intent.ACTION_VIEW, webpage)
    context.startActivity(intent)
 }
 
-private fun dialNumber(context: Context) {
+fun dialNumber(context: Context) {
    val phoneNumber = Uri.parse("tel:111-222-3333")
    val intent = Intent(Intent.ACTION_DIAL, phoneNumber)
    context.startActivity(intent)
 }
 
-private fun viewMap(context: Context) {
+fun noActivityFound(context: Context) {
+   try {
+      val location = Uri.parse("geo:0,0?q=1600+Pennsylvania+Ave+NW,+Washington,+DC")
+
+      // No activity is likely registered for the send action with geo URI
+      val intent = Intent(Intent.ACTION_SEND, location)
+      context.startActivity(intent)
+   }
+   catch (ex: ActivityNotFoundException) {
+      Log.d("TAG", "No activity found")
+   }
+}
+
+fun viewMap(context: Context) {
    val location = Uri.parse("geo:0,0?q=1600+Pennsylvania+Ave+NW,+Washington,+DC")
-   val intent = Intent(Intent.ACTION_VIEW, location)
+   val intent = Intent(Intent.ACTION_SEND, location)
 
    // Hard-coded to only open in Google Maps
    //intent.setPackage("com.google.android.apps.maps")
    context.startActivity(intent)
 }
 
-private fun shareMessage(context: Context) {
+fun shareMessage(context: Context) {
    val intent = Intent(Intent.ACTION_SEND).apply {
       type = "text/plain"
       putExtra(Intent.EXTRA_SUBJECT, "Helpful website")
       putExtra(Intent.EXTRA_TEXT, "https://stackoverflow.com/")
    }
 
-   context.startActivity(
-      Intent.createChooser(intent,"Pet Adoption")
-   )
+   val chooserIntent = Intent.createChooser(intent,"Share URL")
+   context.startActivity(chooserIntent)
 }
 
 @Preview(showBackground = true)
