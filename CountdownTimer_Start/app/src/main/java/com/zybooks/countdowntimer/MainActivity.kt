bf169158b3a3011cf21f32b0834fd303f04a6dda
Frank McCown <fmccown@harding.edu>
2024-05-06 11:04:55 -0500

    Add CountdownTimer_Start project

---
+++ CountdownTimer_Start/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.zybooks.countdowntimer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zybooks.countdowntimer.ui.TimerScreen
+import com.zybooks.countdowntimer.ui.theme.CountdownTimerTheme
+
+class MainActivity : ComponentActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      enableEdgeToEdge()
+      setContent {
+         CountdownTimerTheme(dynamicColor = false) {
+            Surface(
+               modifier = Modifier.fillMaxSize(),
+               color = MaterialTheme.colorScheme.background
+            ) {
+               TimerScreen()
+            }
+         }
+      }
+   }
+}
---
+++ CountdownTimer_Start/app/src/main/java/com/zybooks/countdowntimer/ui/TimerViewModel.kt
@@ -0,0 +1,54 @@
+package com.zybooks.countdowntimer.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class TimerViewModel : ViewModel() {
+   private var timerJob: Job? = null
+
+   // Values selected in time picker
+   var selectedHour by mutableIntStateOf(0)
+      private set
+   var selectedMinute by mutableIntStateOf(0)
+      private set
+   var selectedSecond by mutableIntStateOf(0)
+      private set
+
+   // Total milliseconds when timer starts
+   var totalMillis by mutableLongStateOf(0L)
+      private set
+
+   // Time that remains
+   var remainingMillis by mutableLongStateOf(0L)
+      private set
+
+   // Timer's running status
+   var isRunning by mutableStateOf(false)
+      private set
+
+   fun selectTime(hour: Int, min: Int, sec: Int) {
+      selectedHour = hour
+      selectedMinute = min
+      selectedSecond = sec
+   }
+
+fun startTimer() {
+   // Convert hours, minutes, and seconds to milliseconds
+   totalMillis = (selectedHour * 60 * 60 + selectedMinute * 60 + selectedSecond) * 1000L
+
+   // TODO: Start coroutine that makes the timer count down
+
+}
+
+   fun cancelTimer() {
+      // TODO: Cancel the timer
+   }
+}
