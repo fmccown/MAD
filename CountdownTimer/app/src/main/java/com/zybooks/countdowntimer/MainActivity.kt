d353b692d8d046b07cfa23ea9c488a1ffb0bbbf9
Frank McCown <fmccown@harding.edu>
2024-05-09 17:50:18 -0500

    Changed some function names in CountdownTimer

--- CountdownTimer/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
+++ CountdownTimer/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.zybooks.countdowntimer
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
@@ -19,6 +25,12 @@ import com.zybooks.countdowntimer.ui.theme.CountdownTimerTheme
 class MainActivity : ComponentActivity() {
 
    private val timerViewModel = TimerViewModel()
+
+   private val permissionRequestLauncher =
+      registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+         val message = if (isGranted) "Permission granted" else "Permission NOT granted"
+         Log.i("MainActivity", message)
+      }
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
@@ -33,6 +45,14 @@ class MainActivity : ComponentActivity() {
             }
          }
       }
+
+      // Only need permission to post notifications on Tiramisu and above
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+         if (ActivityCompat.checkSelfPermission(this,
+               Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+            permissionRequestLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+         }
+      }
    }
 
    override fun onStop() {
@@ -40,7 +60,14 @@ class MainActivity : ComponentActivity() {
 
       // Start TimerWorker if the timer is running
       if (timerViewModel.isRunning) {
-         startWorker(timerViewModel.remainingMillis)
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(this,
+                  Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+               startWorker(timerViewModel.remainingMillis)
+            }
+         } else {
+            startWorker(timerViewModel.remainingMillis)
+         }
       }
    }
 
@@ -52,7 +79,6 @@ class MainActivity : ComponentActivity() {
             )
          ).build()
 
-      println("Starting worker")
       WorkManager.getInstance(applicationContext).enqueue(timerWorkRequest)
    }
 }
--- CountdownTimer/app/src/main/java/com/zybooks/countdowntimer/TimerWorker.kt
+++ CountdownTimer/app/src/main/java/com/zybooks/countdowntimer/TimerWorker.kt
@@ -1,16 +1,25 @@
 package com.zybooks.countdowntimer
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.zybooks.countdowntimer.ui.timerText
 import kotlinx.coroutines.delay
 
+const val CHANNEL_ID_TIMER = "channel_timer"
+const val NOTIFICATION_ID = 0
 const val KEY_MILLIS_REMAINING = "com.zybooks.countdowntimer.MILLIS_LEFT"
 
 class TimerWorker(context: Context, parameters: WorkerParameters) :
    CoroutineWorker(context, parameters) {
+
+   private val notificationManager =
+      context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
    override suspend fun doWork(): Result {
       // Get remaining milliseconds from MainActivity
@@ -24,24 +33,44 @@ class TimerWorker(context: Context, parameters: WorkerParameters) :
       // Create notification channel for all notifications
       createTimerNotificationChannel()
 
-      // Create notifications every second until no time remains
+      // Post notifications every second until no time remains
       while (remainingMillis > 0) {
-         createTimerNotification(timerText(remainingMillis))
+         postTimerNotification(timerText(remainingMillis))
          delay(1000)
          remainingMillis -= 1000
       }
 
-      // Send final notification
-      createTimerNotification("Timer is finished!")
+      // Post final notification
+      postTimerNotification("Timer is finished!")
 
       return Result.success()
    }
 
    private fun createTimerNotificationChannel() {
+      // Notification channels only available on O and above
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+         val channel = NotificationChannel(CHANNEL_ID_TIMER, "Timer Channel",
+            NotificationManager.IMPORTANCE_LOW)
+         channel.description = "Displays how much time is left"
 
+         // Register channel
+         notificationManager.createNotificationChannel(channel)
+      }
    }
 
-   private fun createTimerNotification(text: String) {
+   private fun postTimerNotification(text: String) {
+      // Create notification with various properties
+      val notification = NotificationCompat.Builder(applicationContext, CHANNEL_ID_TIMER)
+         .setSmallIcon(android.R.drawable.ic_dialog_info)
+         .setContentTitle(applicationContext.getString(R.string.app_name))
+         .setContentText(text)
+         .setPriority(NotificationCompat.PRIORITY_LOW)
+         .build()
+
+      // Post notification
+      if (notificationManager.areNotificationsEnabled()) {
+         notificationManager.notify(NOTIFICATION_ID, notification)
+      }
 
       Log.d("TimerWorker", text)
    }
