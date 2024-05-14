774ae3ff2b45a7402c7c1a703aab47883deb919a
Frank McCown <fmccown@harding.edu>
2024-05-14 14:32:33 -0500

    Remove unused import

--- ToDoList_Settings/app/src/main/java/com/zybooks/todolist/ui/ToDoScreen.kt
+++ ToDoList_Settings/app/src/main/java/com/zybooks/todolist/ui/ToDoScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Card
