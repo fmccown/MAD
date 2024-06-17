22e98f16ef19157aca9ea6378284cd1ceeb08af5
Frank McCown <fmccown@harding.edu>
2024-06-17 15:43:02 -0500

    Convert StudyHelper_Starter to use save arg navigation

--- StudyHelper_Starter/app/build.gradle.kts
+++ StudyHelper_Starter/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-   alias(libs.plugins.androidApplication)
-   alias(libs.plugins.jetbrainsKotlinAndroid)
+   alias(libs.plugins.android.application)
+   alias(libs.plugins.jetbrains.kotlin.android)
+   alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 android {
@@ -62,6 +63,7 @@ dependencies {
    implementation(libs.androidx.ui.tooling.preview)
    implementation(libs.androidx.material3)
    implementation(libs.androidx.navigation.compose)
+   implementation(libs.kotlinx.serialization.json)
    testImplementation(libs.junit)
    androidTestImplementation(libs.androidx.junit)
    androidTestImplementation(libs.androidx.espresso.core)
--- StudyHelper_Starter/app/src/main/java/com/zybooks/studyhelper/ui/StudyHelperApp.kt
+++ StudyHelper_Starter/app/src/main/java/com/zybooks/studyhelper/ui/StudyHelperApp.kt
@@ -6,12 +6,20 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import com.zybooks.studyhelper.ui.question.QuestionScreen
 import com.zybooks.studyhelper.ui.subject.SubjectScreen
+import kotlinx.serialization.Serializable
 
-enum class StudyScreen {
-   SUBJECT,
-   QUESTION,
+sealed class Routes {
+   @Serializable
+   data object Subject
+
+   @Serializable
+   data class Question(
+      val subjectId: Long,
+      val showLastQuestion: Boolean = false
+   )
 }
 
 @Composable
@@ -20,26 +28,20 @@ fun StudyHelperApp() {
 
    NavHost(
       navController = navController,
-      startDestination = StudyScreen.SUBJECT.name
+      startDestination = Routes.Subject
    ) {
-      composable(route = StudyScreen.SUBJECT.name) {
+      composable<Routes.Subject> {
          SubjectScreen(
             onSubjectClick = { subject ->
-               navController.navigate("${StudyScreen.QUESTION.name}/${subject.id}")
+               navController.navigate(
+                  Routes.Question(subjectId = subject.id)
+               )
             }
          )
       }
-
-      composable(
-         route = "${StudyScreen.QUESTION.name}/{subjectId}",
-
-         // Specify subject ID argument so QuestionViewModel can determine selected subject
-         arguments = listOf(navArgument("subjectId") {
-            type = NavType.LongType
-         })
-      ) { backStackEntry ->
+      composable<Routes.Question> { backStackEntry ->
          QuestionScreen(
-            onUpClick = { navController.popBackStack() },
+            onUpClick = { navController.navigateUp() },
             onAddClick = { },
             onEditClick = { }
          )
