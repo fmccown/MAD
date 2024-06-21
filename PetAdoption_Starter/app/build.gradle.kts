dd941d922ff84db40346b85c7fac5744f0c5ddf4
Frank McCown <fmccown@harding.edu>
2024-06-21 10:18:06 -0500

    Update PetAdoption_Starter to use newer nav library

--- PetAdoption_Starter/app/build.gradle.kts
+++ PetAdoption_Starter/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
    alias(libs.plugins.android.application)
    alias(libs.plugins.jetbrains.kotlin.android)
+   alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 android {
@@ -50,6 +51,8 @@ android {
 }
 
 dependencies {
+   implementation(libs.androidx.navigation.compose)
+   implementation(libs.kotlinx.serialization.json)
    implementation(libs.androidx.lifecycle.viewmodel.compose)
    implementation(libs.androidx.core.ktx)
    implementation(libs.androidx.lifecycle.runtime.ktx)
