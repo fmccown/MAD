5c0aa54289e03ed56bf01b7cbd2b5a1ee76461be
Frank McCown <fmccown@harding.edu>
2024-06-24 09:39:28 -0500

    Update StudyHelper

--- StudyHelper/app/src/main/java/com/zybooks/studyhelper/ui/question/EditQuestionScreen.kt
+++ StudyHelper/app/src/main/java/com/zybooks/studyhelper/ui/question/EditQuestionScreen.kt
@@ -3,7 +3,7 @@ package com.zybooks.studyhelper.ui.question
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -35,7 +35,7 @@ fun EditQuestionScreen(
             modifier = modifier,
             navigationIcon = {
                IconButton(onClick = onUpClick) {
-                  Icon(Icons.Filled.ArrowBack, "Back")
+                  Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
                }
             }
          )
