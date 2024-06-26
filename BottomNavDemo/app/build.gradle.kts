6f7fe798d283607c59bffc8f1e026a1c455a67d6
Frank McCown <fmccown@harding.edu>
2024-06-26 10:04:23 -0500

    Updated build fines in BottomNavDemo

--- BottomNavDemo/app/build.gradle.kts
+++ BottomNavDemo/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-   alias(libs.plugins.androidApplication)
-   alias(libs.plugins.jetbrainsKotlinAndroid)
+   alias(libs.plugins.android.application)
+   alias(libs.plugins.jetbrains.kotlin.android)
+   alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 android {
@@ -50,7 +51,6 @@ android {
 }
 
 dependencies {
-
    implementation(libs.androidx.core.ktx)
    implementation(libs.androidx.lifecycle.runtime.ktx)
    implementation(libs.androidx.activity.compose)
