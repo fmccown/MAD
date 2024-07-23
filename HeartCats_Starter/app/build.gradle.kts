f6151b5274c3622582892a8ddc59891f2398b23f
Frank McCown <fmccown@harding.edu>
2024-07-23 10:52:57 -0500

    Remove dependencies from HeartCats_Starter

--- HeartCats_Starter/app/build.gradle.kts
+++ HeartCats_Starter/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
    alias(libs.plugins.android.application)
    alias(libs.plugins.jetbrains.kotlin.android)
-   id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
 }
 
 android {
@@ -51,11 +50,7 @@ android {
 }
 
 dependencies {
-   implementation("io.coil-kt:coil-compose:2.4.0")
-   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
-   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-   implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-   implementation("com.squareup.okhttp3:okhttp:4.11.0")
+   implementation(libs.androidx.lifecycle.viewmodel.compose)
    implementation(libs.androidx.core.ktx)
    implementation(libs.androidx.lifecycle.runtime.ktx)
    implementation(libs.androidx.activity.compose)
