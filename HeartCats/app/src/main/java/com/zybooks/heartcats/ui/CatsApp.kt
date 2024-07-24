23e88c7e8d50370880a926d9753a799878722559
Frank McCown <fmccown@harding.edu>
2024-07-24 16:14:10 -0500

    Remove unused imports in HeartCats

--- HeartCats/app/src/main/java/com/zybooks/heartcats/ui/CatsApp.kt
+++ HeartCats/app/src/main/java/com/zybooks/heartcats/ui/CatsApp.kt
@@ -25,14 +25,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.zybooks.heartcats.R
 import com.zybooks.heartcats.data.CatImage
 
