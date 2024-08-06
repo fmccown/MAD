d9beb0c840799eb4e8afc1a9a080203d116b0fba
Frank McCown <fmccown@harding.edu>
2024-08-06 16:09:32 -0500

    Remove unused imports from PhotoExpress

--- PhotoExpress/app/src/main/java/com/zybooks/photoexpress/ui/PhotoExpressApp.kt
+++ PhotoExpress/app/src/main/java/com/zybooks/photoexpress/ui/PhotoExpressApp.kt
@@ -16,10 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
