fb93cfbe9440221319e32f4770dba2746dff25e4
Frank McCown <fmccown@harding.edu>
2024-08-06 16:14:27 -0500

    Minor code update

--- PhotoExpress/app/src/main/java/com/zybooks/photoexpress/PhotoExpressApplication.kt
+++ PhotoExpress/app/src/main/java/com/zybooks/photoexpress/PhotoExpressApplication.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import com.zybooks.photoexpress.data.ImageRepository
 
 class PhotoExpressApplication : Application() {
-   // Needed to create ViewModels with the ViewModelProvider.Factory
    lateinit var imageRepo: ImageRepository
 
    // For onCreate() to run, android:name=".PhotoExpressApplication" must
