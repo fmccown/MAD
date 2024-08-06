272eb3b45da6165b9450113ced69dbdef9e1c695
Frank McCown <fmccown@harding.edu>
2024-08-06 15:46:35 -0500

    Change UI in PhotoExpress

--- PhotoExpress/app/src/main/java/com/zybooks/photoexpress/ui/PhotoExpressApp.kt
+++ PhotoExpress/app/src/main/java/com/zybooks/photoexpress/ui/PhotoExpressApp.kt
@@ -2,17 +2,18 @@ package com.zybooks.photoexpress.ui
 
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -20,13 +21,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil.compose.rememberAsyncImagePainter
-import kotlinx.coroutines.launch
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
+import com.zybooks.photoexpress.R
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PhotoExpressApp(
    viewModel: PhotoExpressViewModel = viewModel(
@@ -35,8 +37,7 @@ fun PhotoExpressApp(
 ) {
    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-   val cameraLauncher =
-      rememberLauncherForActivityResult(
+   val cameraLauncher = rememberLauncherForActivityResult(
          ActivityResultContracts.TakePicture()
       ) { success ->
          if (success) {
@@ -47,54 +48,62 @@ fun PhotoExpressApp(
    var sliderPosition by remember { mutableFloatStateOf(100f) }
    val coroutineScope = rememberCoroutineScope()
 
-   Column(
-      verticalArrangement = Arrangement.Center,
-      modifier = Modifier.padding(16.dp)
-   ) {
-      Row(
-         modifier = Modifier.fillMaxWidth(),
-         horizontalArrangement = Arrangement.SpaceBetween
-      ) {
-         Button(onClick = {
-            val photoUri = viewModel.takePhoto()
-            sliderPosition = 100f
-            cameraLauncher.launch(photoUri)
-         }) {
-            Text("Take Picture")
-         }
-         if (uiState.photoVisible) {
-            Button(
-               onClick = {
-                  coroutineScope.launch {
-                     viewModel.saveAlteredPhoto()
-                  }
-               },
-               enabled = !uiState.photoSaved
-            ) {
-               Text("Save")
+   Scaffold(
+      topBar = {
+         TopAppBar(
+            title = { Text("Photo Express") },
+            actions = {
+               IconButton(onClick = {
+                  val photoUri = viewModel.takePhoto()
+                  sliderPosition = 100f
+                  cameraLauncher.launch(photoUri)
+               }) {
+                  Icon(
+                     painter = painterResource(R.drawable.camera),
+                     contentDescription = "Take Photo"
+                  )
+               }
+               IconButton(
+                  onClick = {
+                     coroutineScope.launch {
+                        viewModel.saveAlteredPhoto()
+                     }
+                  },
+                  enabled = !uiState.photoSaved
+               ) {
+                  Icon(
+                     painter = painterResource(R.drawable.save),
+                     contentDescription = "Save"
+                  )
+               }
             }
+         )
+      }
+   ) { innerPadding ->
+      Column(
+         verticalArrangement = Arrangement.Center,
+         modifier = Modifier.padding(innerPadding)
+      ) {
+         if (uiState.photoVisible) {
+            AsyncImage(
+               model = uiState.photoUri,
+               contentDescription = null,
+               modifier = Modifier.fillMaxHeight(0.9f),
+               colorFilter = uiState.colorFilter
+            )
+            Slider(
+               value = sliderPosition,
+               valueRange = 0f..200f,
+               onValueChange = {
+                  sliderPosition = it
+                  viewModel.changeBrightness(sliderPosition)
+               },
+               modifier = Modifier.weight(1f)
+            )
          }
-      }
-
-      if (uiState.photoVisible) {
-         AsyncImage(
-            model = uiState.photoUri,
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-            colorFilter = uiState.colorFilter
-         )
-         Slider(
-            value = sliderPosition,
-            valueRange = 0f..200f,
-            onValueChange = {
-               sliderPosition = it
-               viewModel.changeBrightness(sliderPosition)
-            },
-            modifier = Modifier.weight(1f)
-         )
-      }
-      else {
-         Spacer(Modifier.weight(1f))
+         else {
+            Spacer(Modifier.weight(1f))
+         }
       }
    }
 }
