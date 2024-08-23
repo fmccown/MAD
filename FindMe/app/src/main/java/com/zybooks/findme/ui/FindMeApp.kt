9ce4ff4a6c7a376e0d4429270219697298bf8e26
Frank McCown <fmccown@harding.edu>
2024-08-23 17:34:15 -0500

    Major changes to FindMe

--- FindMe/app/src/main/java/com/zybooks/findme/ui/FindMeApp.kt
+++ FindMe/app/src/main/java/com/zybooks/findme/ui/FindMeApp.kt
@@ -2,8 +2,6 @@ package com.zybooks.findme.ui
 
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.location.Location
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
@@ -16,22 +14,20 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
-import com.google.android.gms.maps.CameraUpdate
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.tasks.CancellationTokenSource
-import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
-import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import kotlinx.coroutines.tasks.await
 
-val singapore = LatLng(1.3588227, 103.8742114)
-
-val defaultCameraPosition = CameraPosition.fromLatLngZoom(singapore, 11f)
+val startingLatLong = LatLng(0.0, 0.0)
+val defaultCameraPosition = CameraPosition.fromLatLngZoom(startingLatLong, 11f)
 
 @SuppressLint("MissingPermission")
 @Composable
@@ -41,47 +37,38 @@ fun FindMeApp() {
       LocationServices.getFusedLocationProviderClient(context)
    }
 
-
-
-   //var currentLocation by remember { mutableStateOf<Location?>(null) }
-
-   var cameraPositionState = rememberCameraPositionState { defaultCameraPosition }
-
-   var mapProperties by remember {
-      mutableStateOf(MapProperties(mapType = MapType.NORMAL))
-   }
-   var uiSettings by remember { mutableStateOf(MapUiSettings(compassEnabled = false)) }
-
+   var currentLatLng by remember { mutableStateOf(startingLatLong) }
+   val cameraPositionState = rememberCameraPositionState { defaultCameraPosition }
 
    GoogleMap(
       cameraPositionState = cameraPositionState,
-      properties = mapProperties,
-      uiSettings = uiSettings,
-      onPOIClick = {
-         Log.d("FindMeApp", "POI clicked: ${it.name}")
-      },
+      properties = MapProperties(mapType = MapType.NORMAL)
    ) {
-
+      if (currentLatLng != startingLatLong) {
+         Marker(
+            state = MarkerState(currentLatLng),
+            title = "Here You Are!"
+         )
+      }
    }
 
-   /*
    LaunchedEffect(Unit) {
       val currentLocation = locationClient.getCurrentLocation(
          Priority.PRIORITY_HIGH_ACCURACY,
          CancellationTokenSource().token).await()
 
       if (currentLocation != null) {
-         println("Current location " +
-               "lat: ${currentLocation.latitude} " +
-               "long: ${currentLocation.longitude}")
+         currentLatLng = LatLng(currentLocation.latitude, currentLocation.longitude)
 
-         cameraPositionState.move(
-            CameraUpdateFactory.newLatLng(
-               LatLng(currentLocation.latitude, currentLocation.longitude)
-            )
+         cameraPositionState.animate(
+            update = CameraUpdateFactory.newLatLngZoom(
+               LatLng(currentLatLng.latitude, currentLatLng.longitude),
+               15f
+            ),
+            durationMs = 1000
          )
       }
-   }*/
+   }
 }
 
 @Composable
