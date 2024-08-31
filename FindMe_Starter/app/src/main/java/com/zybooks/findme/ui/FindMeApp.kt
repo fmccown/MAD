0c97eeae29c17e7e33bbcb1e954fba018bd1632b
Frank McCown <fmccown@harding.edu>
2024-08-31 15:16:04 -0500

    Run on IO coroutine in FindMe_Starter

--- FindMe_Starter/app/src/main/java/com/zybooks/findme/ui/FindMeApp.kt
+++ FindMe_Starter/app/src/main/java/com/zybooks/findme/ui/FindMeApp.kt
@@ -2,6 +2,7 @@ package com.zybooks.findme.ui
 
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
@@ -15,7 +16,9 @@ import androidx.core.content.ContextCompat
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 @SuppressLint("MissingPermission")
 @Composable
@@ -26,9 +29,13 @@ fun FindMeApp() {
    }
 
    LaunchedEffect(Unit) {
-      val currentLocation = locationClient.getCurrentLocation(
-         Priority.PRIORITY_HIGH_ACCURACY,
-         CancellationTokenSource().token).await()
+      var currentLocation: Location?
+      withContext(Dispatchers.IO) {
+         currentLocation = locationClient.getCurrentLocation(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            CancellationTokenSource().token
+         ).await()
+      }
 
       currentLocation?.let {
          println("Current location: lat=${it.latitude}, long=${it.longitude}")
