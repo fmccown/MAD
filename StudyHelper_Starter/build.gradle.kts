36fa54d213e33c20d4606ba93f2bc02841b24453
Frank McCown <fmccown@harding.edu>
2025-04-01 11:11:56 -0500

    Comment about ksp version

--- StudyHelper_Starter/build.gradle.kts
+++ StudyHelper_Starter/build.gradle.kts
@@ -3,5 +3,7 @@ plugins {
    alias(libs.plugins.android.application) apply false
    alias(libs.plugins.jetbrains.kotlin.android) apply false
    alias(libs.plugins.jetbrains.kotlin.serialization) apply false
+
+   // Use this version instead of 2.X listed in the zyBook
    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 }
